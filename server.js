a17495831dd6389b776478ab868c3f53009c53a6
Glitch (hall500-project-exercisetracker) <none>
2020-08-19 23:44:49 +0000

    🐆📚 Checkpoint ./server.js:10219482/55

--- server.js
+++ server.js
@@ -93,9 +93,9 @@ app.get("/api/exercise/log", function(req, res){
   if(userId == "") return res.send("unknown userId")
   User.find({ _id: userId }).select({ __v: 0 }).then(function(user){
     var exe = Exercise.find({ username: user[0].username })
-    if(from != "" && to != "") Exercise.find({ username: user[0].username, date: { $gte: from, $lte: to} })
+    if(from != "" && to != "") exe = Exercise.find({ username: user[0].username, date: { $gte: from, $lte: to} })
     else if(from != "") exe = Exercise.find({ username: user[0].username, date: { $gte: from } })
-    elseif(to != "") exe = Exercise.find({ username: user[0].username, date: { $lte: to} })
+    else if(to != "") exe = Exercise.find({ username: user[0].username, date: { $lte: to} })
     if(lim > 0) exe = exe.limit(lim)
     exe.select({ _id: 0, __v: 0, username: 0 }).then(function(exercises){
       return res.json({
